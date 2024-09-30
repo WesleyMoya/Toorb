7097f7ccad995dc234f5e31ea5ce7b2463c67d92
Wesley Leandro Moya da Silva <wesleymoya2000@gmail.com>
2024-09-30 16:07:48 -0300

    Commit 30/09/24
    . Ajuste no Menu Inicial
    . Interação com NPC/Objetos (base)
    . Aumento do Mapa
    . Troca do Icone de Vida

--- Assets/Menu/MenuPrincipalManager.cs
+++ Assets/Menu/MenuPrincipalManager.cs
@@ -2,16 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MenuPrincipalManager : MonoBehaviour
 {
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+    [SerializeField] private GameObject imagemMenuInicial;
+
     public void Jogar()
     {
-        SceneManager.LoadScene("Jogo1");
+        // Mudar para adaptar o LoadGame depois !!!
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+    public void AbrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+        imagemMenuInicial.SetActive(true);
+    }
+    public void FecharOpcoes()
+    {
+        painelMenuInicial.SetActive(true);
+        painelOpcoes.SetActive(false);
+        imagemMenuInicial.SetActive(false);
     }
 
     public void Sair()
     {
-        
+        //apenas para teste
+        Debug.Log("Saiu do jogo");
+
+        Application.Quit();
     }
 }
