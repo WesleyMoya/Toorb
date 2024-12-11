701f8a05ce58fc25c9de5864be695e3d37f4e636
Wesley Leandro Moya da Silva <103061791+WesleyMoya@users.noreply.github.com>
2024-12-10 21:33:30 -0300

    Commit 10/12/24 2

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,6 +4,18 @@ public class DontDestroy : MonoBehaviour
 {
     void Awake()
     {
+        // Verifica se já existe um objeto com o mesmo nome na cena
+        DontDestroy[] existingObjects = FindObjectsOfType<DontDestroy>();
+        foreach (DontDestroy obj in existingObjects)
+        {
+            if (obj.gameObject.name == this.gameObject.name && obj != this)
+            {
+                Destroy(this.gameObject); // Destroi a instância atual se já existir outra com o mesmo nome
+                return; // Sai do método para evitar a execução adicional
+            }
+        }
+
+        // Se não houver duplicatas, mantém o objeto
         DontDestroyOnLoad(this.gameObject);
     }
 }
